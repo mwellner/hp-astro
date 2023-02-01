4c5db608d1ee2b0e244ac18cde4801dd70bdbc64
Mathias Wellner <mathias@mwellner.de>
2023-02-01 17:22:42 +0100

    add blog posts and responsive images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    mdx(),
+  ],
+});
--- src/pages/posts/post-1.md
+++ src/pages/posts/post-1.md
@@ -1,7 +1,7 @@
 ---
 layout: ../../layouts/MarkdownPostLayout.astro
 title: "My First Blog Post"
-pubDate: 2023-01-25
+date: 2023-01-25
 description: "This is the first post of my new Astro blog."
 author: Mathias Wellner
 image:
