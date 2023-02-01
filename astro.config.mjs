7d361b084d934dad8b8537a3e27fec8780b7efae
Mathias Wellner <mathias@mwellner.de>
2023-02-01 20:13:00 +0100

    work on translations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,22 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: "dark-plus",
+      langs: ["sql", "javascript", "c#"],
+      wrap: true,
+    },
+  },
   integrations: [
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
     mdx(),
+    astroI18next(),
   ],
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 export async function get() {
   return rss({
     title: "Mathias Wellner",
-    description: "My journey learning Astro",
+    description: "Persönliche Webseite",
     site: "https://mwellner.de",
     items: await pagesGlobToRssItems(import.meta.glob("./**/*.mdx")),
     customData: `<language>de-de</language>`,
