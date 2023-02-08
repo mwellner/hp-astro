f8bd34f09ec1ca3bafd9ae1de65fa2a6b9d8af4d
Mathias Wellner <mathias@mwellner.de>
2023-02-08 18:16:37 +0100

    transform to content collection

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+import { z, defineCollection } from "astro:content";
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: z
+      .object({
+        src: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
+    pubDate: z.date(),
+    language: z.enum(["en", "de"]),
+    author: z.string().default("Mathias Wellner"),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
---
+++ src/pages/index.xml.js
@@ -0,0 +1,18 @@
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import { getCollection } from "astro:content";
+
+export async function get() {
+  const posts = await getCollection("blog");
+  return rss({
+    title: "Mathias Wellner",
+    description: "Persönliche Webseite",
+    site: "https://mwellner.de",
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `${post.slug}`,
+    })),
+    customData: `<language>de-de</language>`,
+  });
+}
