f808d31fc335dff4787dfbbe11278a80b62d0882
Mathias Wellner <mathias@mwellner.de>
2023-02-12 17:40:26 +0100

    fix url for english posts in rss feed

--- src/pages/index.xml.js
+++ src/pages/index.xml.js
@@ -1,4 +1,4 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
 export async function get() {
@@ -11,7 +11,7 @@ export async function get() {
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      link: `${post.slug}`,
+      link: `${post.data.language === "en" ? "en/" : ""}${post.slug}`,
     })),
     customData: `<language>de-de</language>`,
   });
