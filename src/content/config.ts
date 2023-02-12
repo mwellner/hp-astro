9b289f4065cac6fb61bb4fab1d2b84f718e06aeb
Mathias Wellner <mathias@mwellner.de>
2023-02-12 12:37:13 +0100

    add seo and descriptions

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,6 +2,7 @@ import { z, defineCollection } from "astro:content";
 const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
+    description: z.string().optional(),
     tags: z.array(z.string()),
     image: z
       .object({
