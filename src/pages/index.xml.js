92946d91c4bc1b96504f34daa5aa1a806c0a5672
Mathias Wellner <mathias@mwellner.de>
2023-02-15 22:53:42 +0100

    touch up rss feed

--- src/pages/index.xml.js
+++ src/pages/index.xml.js
@@ -2,17 +2,20 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
 export async function get() {
-  const posts = await getCollection("blog");
+  let posts = await getCollection("blog", ({ data }) => data.language === "de");
+  posts = posts.slice(-25);
   return rss({
     title: "Mathias Wellner",
     description: "Persönliche Webseite",
     site: "https://mwellner.de",
-    items: posts.map((post) => ({
-      title: post.data.title,
-      date: post.data.date,
-      description: post.data.description,
-      link: `${post.data.language === "en" ? "en/" : ""}${post.slug}`,
-    })),
+    items: posts.map((post) => {
+      return {
+        title: post.data.title,
+        pubDate: post.data.date,
+        description: post.data.description || "",
+        link: `${post.data.language === "en" ? "en/" : ""}${post.slug}`,
+      };
+    }),
     customData: `<language>de-de</language>`,
   });
 }
