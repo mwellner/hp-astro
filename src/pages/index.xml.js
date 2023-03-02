788791b12c4d5e67965536698bbe274d4b464091
Mathias Wellner <mathias@mwellner.de>
2023-03-02 20:56:53 +0100

    include english rss items

--- src/pages/index.xml.js
+++ src/pages/index.xml.js
@@ -2,7 +2,7 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
 export async function get() {
-  let posts = await getCollection("blog", ({ data }) => data.language === "de");
+  let posts = await getCollection("blog");
   posts = posts.slice(-25);
   return rss({
     title: "Mathias Wellner",
@@ -13,9 +13,11 @@ export async function get() {
         title: post.data.title,
         pubDate: post.data.date,
         description: post.data.description || "",
+        customData: `<language>${
+          post.data.language === "en" ? "en-us" : "de-de"
+        }</language>`,
         link: `${post.data.language === "en" ? "en/" : ""}${post.slug}`,
       };
     }),
-    customData: `<language>de-de</language>`,
   });
 }
